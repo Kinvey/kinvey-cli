8c17aada5e399a9aefa34b5f9470698acf6fe7e5
Sam Levin <sjl2024@users.noreply.github.com>
2017-02-16 22:47:13 -0500

    BACK-1936: job.js

--- test/job.js
+++ test/job.js
@@ -16,12 +16,22 @@
 const sinon = require('sinon');
 const command = require('./fixtures/command.js');
 const service = require('../lib/service.js');
-const deploy = require('../cmd/deploy.js');
+const logger = require('../lib/logger.js');
 const pkg = require('../package.json');
 const project = require('../lib/project.js');
+const job = require('../cmd/job.js');
 const user = require('../lib/user.js');
 
-describe(`./${pkg.name} deploy`, () => {
+describe(`./${pkg.name} job`, () => {
+  before('configure', () => {
+    project.app = project.service = '123';
+    project.schemaVersion = 1;
+    return project.lastJobId = 'abcdef';
+  });
+  after('configure', () => {
+    project.app = project.service = project.schemaVersion = null;
+  });
+
   before('user', () => {
     return sinon.stub(user, 'setup').callsArg(1);
   });
@@ -31,7 +41,6 @@ describe(`./${pkg.name} deploy`, () => {
   after('user', () => {
     return user.setup.restore();
   });
-
   before('project', () => {
     return sinon.stub(project, 'restore').callsArg(0);
   });
@@ -41,48 +50,39 @@ describe(`./${pkg.name} deploy`, () => {
   after('project', () => {
     return project.restore.restore();
   });
-
-  before('validate', () => {
-    return sinon.stub(service, 'validate').callsArg(1);
+  before('service', () => {
+    return sinon.stub(service, 'jobStatus').callsArg(1);
   });
-  afterEach('validate', () => {
-    return service.validate.reset();
+  afterEach('service', () => {
+    return service.jobStatus.reset();
   });
-  after('validate', () => {
-    return service.validate.restore();
+  after('service', () => {
+    return service.jobStatus.restore();
   });
-
-  before('deploy', () => {
-    return sinon.stub(service, 'deploy').callsArg(1);
-  });
-  afterEach('deploy', () => {
-    return service.deploy.reset();
-  });
-  after('deploy', () => {
-    return service.deploy.restore();
-  });
-
   it('should setup the user.', (cb) => {
-    return deploy.call(command, command, (err) => {
+    return job('123', command, (err) => {
       expect(user.setup).to.be.calledOnce;
       return cb(err);
     });
   });
   it('should restore the project.', (cb) => {
-    return deploy.call(command, command, (err) => {
+    return job('123', command, (err) => {
       expect(project.restore).to.be.calledOnce;
       return cb(err);
     });
   });
-  it('should validate the service.', (cb) => {
-    return deploy.call(command, command, (err) => {
-      expect(service.validate).to.be.calledOnce;
+  it('should print the current job status.', (cb) => {
+    const jobId = '123';
+    return job(jobId, command, (err) => {
+      expect(service.jobStatus).to.be.calledOnce;
+      expect(service.jobStatus).to.be.calledWith(jobId);
       return cb(err);
     });
   });
-  return it('should deploy the service.', (cb) => {
-    return deploy.call(command, command, (err) => {
-      expect(service.deploy).to.be.calledOnce;
+  return it('should print the current job status when called without an id.', (cb) => {
+    return job(null, command, (err) => {
+      expect(service.jobStatus).to.be.calledOnce;
+      expect(service.jobStatus).to.be.calledWith(null);
       return cb(err);
     });
   });

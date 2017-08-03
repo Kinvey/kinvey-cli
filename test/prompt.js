b33a188f2fb511858938c42243a2f055ef127c96
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2017-08-03 17:16:41 +0300

    BACK-2704: Boost prompt tests coverage to 100%

--- test/prompt.js
+++ test/prompt.js
@@ -13,8 +13,21 @@
  * contents is a violation of applicable laws.
  */
 
+const sinon = require('sinon');
+const inquirer = require('inquirer');
+
 const prompt = require('../lib/prompt');
+const logger = require('./../lib/logger');
 const PromptMessages = require('./../lib/constants').PromptMessages;
+const InfoMessages = require('./../lib/constants').InfoMessages;
+
+function getInquirerPromptStub(sandbox, answers) {
+  return sandbox.stub(inquirer, 'prompt', (questions, cb) => {
+    setTimeout(() => {
+      cb(answers);
+    }, 0);
+  });
+}
 
 describe('prompt', () => {
   describe('validate e-mail address', () => {
@@ -59,6 +72,123 @@ describe('prompt', () => {
       it(`should return '${x.expectedResult}' when ${x.value}`, () => {
         const actualResult = prompt.validateMfaToken(x.value);
         expect(actualResult).to.eql(x.expectedResult);
+      });
+    });
+  });
+
+  describe('get', () => {
+    const sandbox = sinon.sandbox.create();
+    const dataValue = 'someAnswerValue';
+    let loggerDebugStub;
+
+    before('setupCommonStubs', () => {
+      loggerDebugStub = sandbox.stub(logger, 'debug');
+    });
+
+    afterEach(() => {
+      loggerDebugStub.reset();
+    });
+
+    after(() => {
+      sandbox.restore();
+    });
+
+    const testCases = [
+      {
+        msg: 'should provide an app',
+        methodName: 'getApp',
+        expectedInfoMsg: InfoMessages.APP_PROMPTING,
+        nameForPrompt: 'app'
+      },
+      {
+        msg: 'should provide app or org',
+        methodName: 'getAppOrOrg',
+        expectedInfoMsg: InfoMessages.APP_OR_ORG_PROMPTING,
+        nameForPrompt: 'option'
+      },
+      {
+        msg: 'should provide an org',
+        methodName: 'getOrg',
+        expectedInfoMsg: InfoMessages.ORG_PROMPTING,
+        nameForPrompt: 'org'
+      },
+      {
+        msg: 'should provide a service',
+        methodName: 'getService',
+        expectedInfoMsg: InfoMessages.SERVICE_PROMPTING,
+        nameForPrompt: 'service'
+      },
+      {
+        msg: 'should provide 2FA token',
+        methodName: 'getTwoFactorToken',
+        expectedInfoMsg: InfoMessages.TWO_FACTOR_TOKEN_PROMPTING,
+        nameForPrompt: 'mfaToken'
+      },
+    ];
+
+    testCases.forEach((x) => {
+      describe(x.methodName, () => {
+        let inquirerPromptStub;
+
+        before(() => {
+          inquirerPromptStub = getInquirerPromptStub(sandbox, { [x.nameForPrompt]: dataValue });
+        });
+
+        afterEach(() => {
+          inquirerPromptStub.reset();
+        });
+
+        after(() => {
+          inquirerPromptStub.restore();
+        });
+
+        it(x.msg, (cb) => {
+          prompt[x.methodName]([], (err, actualData) => {
+            expect(err).to.not.exist;
+            expect(loggerDebugStub).to.be.calledOnce.and.to.be.calledWith(x.expectedInfoMsg);
+            expect(inquirerPromptStub).to.be.calledOnce;
+            expect(actualData).to.exist.and.to.equal(dataValue);
+            cb();
+          });
+        });
+      });
+    });
+
+    describe('get e-mail and password', () => {
+      let inquirerPromptStub;
+
+      afterEach(() => {
+        inquirerPromptStub.restore();
+      });
+
+      it('should return the already provided ones if no answers from prompt', (cb) => {
+        inquirerPromptStub = getInquirerPromptStub(sandbox, {});
+
+        const expectedEmail = 'janeDoe@email.com';
+        const expectedPassword = 'youShallNotPass';
+        prompt.getEmailPassword(expectedEmail, expectedPassword, (err, actualEmail, actualPassword) => {
+          expect(err).to.not.exist;
+          expect(loggerDebugStub).to.be.calledOnce.and.to.be.calledWith(InfoMessages.EMAIL_PASSWORD_PROMPTING);
+          expect(inquirerPromptStub).to.be.calledOnce;
+          expect(actualEmail).to.equal(expectedEmail);
+          expect(actualPassword).to.equal(expectedPassword);
+          cb();
+        });
+      });
+
+      it('should return email and password if answers from prompt', (cb) => {
+        const expectedEmail = 'janeDoe@email.com';
+        const expectedPassword = 'youShallNotPass';
+        inquirerPromptStub = getInquirerPromptStub(sandbox, { email: expectedEmail, password: expectedPassword });
+
+        prompt.getEmailPassword(null, null, (err, actualEmail, actualPassword) => {
+          expect(err).to.not.exist;
+          expect(loggerDebugStub).to.be.calledOnce.and.to.be.calledWith(InfoMessages.EMAIL_PASSWORD_PROMPTING);
+          expect(inquirerPromptStub).to.be.calledOnce;
+          expect(actualEmail).to.equal(expectedEmail);
+          expect(actualPassword).to.equal(expectedPassword);
+          cb();
+        });
       });
     });
   });

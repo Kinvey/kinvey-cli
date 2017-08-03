8210a0005cd480fde7a388a0cf906e2d5e82822e
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2017-08-03 13:20:47 +0300

    BACK-2704: Add test cases for email and 2fa token validation; refactor

--- test/prompt.js
+++ test/prompt.js
@@ -14,30 +14,52 @@
  */
 
 const prompt = require('../lib/prompt');
+const PromptMessages = require('./../lib/constants').PromptMessages;
 
 describe('prompt', () => {
-  it('should validate an email address', () => {
-    let result = prompt.validateEmail('abc');
-    expect(result).to.eql('Please enter a valid e-mail address.');
-    result = prompt.validateEmail('');
-    expect(result).to.eql('Please enter a valid e-mail address.');
-    result = prompt.validateEmail('abc@email.com');
-    expect(result).to.eql(true);
+  describe('validate e-mail address', () => {
+    const failureResult = PromptMessages.INVALID_EMAIL_ADDRESS;
+    const testCases = [
+      { value: 'abc@email.com', expectedResult: true },
+      { value: 'jane+doe@domain.com', expectedResult: true },
+      { value: 'email@domain.co.jp', expectedResult: true },
+      { value: 'email@123.123.123.123', expectedResult: true },
+      { value: 'abc', expectedResult: failureResult },
+      { value: '', expectedResult: failureResult },
+      { value: '#@%^%#$@#$@#.com', expectedResult: failureResult },
+      { value: '@domain.com', expectedResult: failureResult },
+    ];
+
+    testCases.forEach((x) => {
+      it(`should return '${x.expectedResult}' when ${x.value}`, () => {
+        const actualResult = prompt.validateEmail(x.value);
+        expect(actualResult).to.eql(x.expectedResult);
+      });
+    });
   });
-  it('should validate a 2FA token', () => {
-    let result = prompt.validateMfaToken('abc');
-    expect(result).to.eql('Please enter a valid 2FA token (6 digits).');
-    result = prompt.validateMfaToken('abcdef');
-    expect(result).to.eql('Please enter a valid 2FA token (6 digits).');
-    result = prompt.validateMfaToken('123');
-    expect(result).to.eql('Please enter a valid 2FA token (6 digits).');
-    result = prompt.validateMfaToken('1234567');
-    expect(result).to.eql('Please enter a valid 2FA token (6 digits).');
-    result = prompt.validateMfaToken('_asdfaih#%$@NUG@A');
-    expect(result).to.eql('Please enter a valid 2FA token (6 digits).');
-    result = prompt.validateMfaToken('');
-    expect(result).to.eql('Please enter a valid 2FA token (6 digits).');
-    result = prompt.validateMfaToken('123456');
-    expect(result).to.eql(true);
+
+  describe('validate 2FA token', () => {
+    const failureResult = PromptMessages.INVALID_MFA_TOKEN;
+    const testCases = [
+      { value: '012345', expectedResult: true },
+      { value: '000000', expectedResult: true },
+      { value: '      ', expectedResult: failureResult },
+      { value: '123456 ', expectedResult: failureResult },
+      { value: ' 123456', expectedResult: failureResult },
+      { value: 'abc', expectedResult: failureResult },
+      { value: '', expectedResult: failureResult },
+      { value: 'abcdef', expectedResult: failureResult },
+      { value: '12345', expectedResult: failureResult },
+      { value: '1234567', expectedResult: failureResult },
+      { value: '_asdfaih#%$@NUG@A', expectedResult: failureResult },
+      { value: null, expectedResult: failureResult },
+    ];
+
+    testCases.forEach((x) => {
+      it(`should return '${x.expectedResult}' when ${x.value}`, () => {
+        const actualResult = prompt.validateMfaToken(x.value);
+        expect(actualResult).to.eql(x.expectedResult);
+      });
+    });
   });
 });

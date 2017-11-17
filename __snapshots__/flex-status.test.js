11861a4ce0e27717d26e7b6d965d574cd610b62c
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2017-11-17 14:45:58 +0200

    BACK-2779: Rename test files for better snapshots structure

--- __snapshots__/flex-status.test.js
+++ __snapshots__/flex-status.test.js
@@ -20,6 +20,34 @@ error:  DataLinkNotFound: The specified data link could not be found.
 
 `
 
+exports['flex status by specifying a profile when valid project is set without serviceId as an option should succeed 1'] = `
+debug:  Checking for package updates
+debug:  Request:  GET http://localhost:3234/v2/data-links/12378kdl2/status
+debug:  Response: GET http://localhost:3234/v2/data-links/12378kdl2/status 200
+Status of FSR service 12378kdl2 (TestKinveyDatalink)
+
+  Status:   ONLINE [v1.4.2]
+
+  Requested on:   Wed Nov 01 2017 10:42:31 GMT+0200 (FLE Standard Time)
+  Deployed by:   davy.jones@mail.com (Davy Jones)
+
+
+`
+
+exports['flex status by specifying a profile when invalid project is set with existent serviceId as an option should succeed 1'] = `
+debug:  Checking for package updates
+debug:  Request:  GET http://localhost:3234/v2/data-links/12378kdl2/status
+debug:  Response: GET http://localhost:3234/v2/data-links/12378kdl2/status 200
+Status of FSR service 12378kdl2 (Service name is not available.)
+
+  Status:   ONLINE [v1.4.2]
+
+  Requested on:   Wed Nov 01 2017 10:42:31 GMT+0200 (FLE Standard Time)
+  Deployed by:   davy.jones@mail.com (Davy Jones)
+
+
+`
+
 exports['flex status by not specifying profile nor credentials when one profile and existent serviceId should succeed 1'] = `
 debug:  Checking for package updates
 debug:  Request:  GET http://localhost:3234/v2/data-links/12378kdl2/status
@@ -31,6 +59,25 @@ Status of FSR service 12378kdl2 (Service name is not available.)
   Requested on:   Wed Nov 01 2017 10:42:31 GMT+0200 (FLE Standard Time)
   Deployed by:   davy.jones@mail.com (Davy Jones)
 
+
+`
+
+exports['flex status by not specifying profile nor credentials when several profiles and existent serviceId should fail 1'] = `
+bincli.js flex status
+
+Options:
+  --version                 Show version number                        [boolean]
+  --email                   E-mail address of your Kinvey account       [string]
+  --password                Password of your Kinvey account             [string]
+  --host                    Host of the Kinvey service                  [string]
+  --profile                 Profile to use                              [string]
+  --silent                  Do not output anything                     [boolean]
+  --suppress-version-check  Do not check for package updates           [boolean]
+  --verbose                 Output debug messages                      [boolean]
+  -h, --help                Show help                                  [boolean]
+  --serviceId               Service ID                                  [string]
+
+You must be authenticated. (CHANGE ERR MSG)
 
 `
 
@@ -71,52 +118,5 @@ debug:  Checking for package updates
 debug:  Request:  POST http://localhost:3234/session
 debug:  Response: POST http://localhost:3234/session 401
 error:  InvalidCredentials: Credentials are invalid. Please authenticate.
-
-`
-
-exports['flex status by not specifying profile nor credentials when several profiles and existent serviceId should fail 1'] = `
-bincli.js flex status
-
-Options:
-  --version                 Show version number                        [boolean]
-  --email                   E-mail address of your Kinvey account       [string]
-  --password                Password of your Kinvey account             [string]
-  --host                    Host of the Kinvey service                  [string]
-  --profile                 Profile to use                              [string]
-  --silent                  Do not output anything                     [boolean]
-  --suppress-version-check  Do not check for package updates           [boolean]
-  --verbose                 Output debug messages                      [boolean]
-  -h, --help                Show help                                  [boolean]
-  --serviceId               Service ID                                  [string]
-
-You must be authenticated. (CHANGE ERR MSG)
-
-`
-
-exports['flex status by specifying a profile when valid project is set without serviceId as an option should succeed 1'] = `
-debug:  Checking for package updates
-debug:  Request:  GET http://localhost:3234/v2/data-links/12378kdl2/status
-debug:  Response: GET http://localhost:3234/v2/data-links/12378kdl2/status 200
-Status of FSR service 12378kdl2 (TestKinveyDatalink)
-
-  Status:   ONLINE [v1.4.2]
-
-  Requested on:   Wed Nov 01 2017 10:42:31 GMT+0200 (FLE Standard Time)
-  Deployed by:   davy.jones@mail.com (Davy Jones)
-
-
-`
-
-exports['flex status by specifying a profile when invalid project is set with existent serviceId as an option should succeed 1'] = `
-debug:  Checking for package updates
-debug:  Request:  GET http://localhost:3234/v2/data-links/12378kdl2/status
-debug:  Response: GET http://localhost:3234/v2/data-links/12378kdl2/status 200
-Status of FSR service 12378kdl2 (Service name is not available.)
-
-  Status:   ONLINE [v1.4.2]
-
-  Requested on:   Wed Nov 01 2017 10:42:31 GMT+0200 (FLE Standard Time)
-  Deployed by:   davy.jones@mail.com (Davy Jones)
-
 
 `
--- __snapshots__/profile-delete.test.js
+++ __snapshots__/profile-delete.test.js
@@ -1,4 +1,4 @@
-exports['profile delete by existent name when there are several should delete only one 1'] = `
+exports['profile delete by existent name when there is only one should succeed 1'] = `
 debug:  Checking for package updates
 debug:  Writing JSON to file globalSetupPath
 debug:  Writing contents to file globalSetupPath
@@ -6,7 +6,15 @@ info:  Successfully deleted profile with name 'testProfileDelete'.
 
 `
 
-exports['profile delete by existent name when there is only one should succeed 1'] = `
+exports['profile delete by existent name when it is the active profile should succeed and clear active 1'] = `
+debug:  Checking for package updates
+debug:  Writing JSON to file globalSetupPath
+debug:  Writing contents to file globalSetupPath
+info:  Successfully deleted profile with name 'activeAndMustBeDeleted'.
+
+`
+
+exports['profile delete by existent name when there are several should delete only one 1'] = `
 debug:  Checking for package updates
 debug:  Writing JSON to file globalSetupPath
 debug:  Writing contents to file globalSetupPath
@@ -41,13 +49,5 @@ Options:
   -h, --help                Show help                                  [boolean]
 
 Not enough non-option arguments: got 0, need at least 1
-
-`
-
-exports['profile delete by existent name when it is the active profile should succeed and clear active 1'] = `
-debug:  Checking for package updates
-debug:  Writing JSON to file globalSetupPath
-debug:  Writing contents to file globalSetupPath
-info:  Successfully deleted profile with name 'activeAndMustBeDeleted'.
 
 `

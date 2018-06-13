b30b15f2cd86c17b19f694c89355fafabf9af804
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2018-06-13 10:15:41 +0300

    CLI-81: Add service validation, more tests

--- __snapshots__/schema-validator.test.js
+++ __snapshots__/schema-validator.test.js
@@ -10,4 +10,10 @@ exports['schema validator env with invalid env should fail 1'] = `
 	scheduledCode.end0.interval: "interval" must be one of [weekly, daily, hourly, 30-minutes, 10-minutes, 5-minutes, 1-minute]
 	scheduledCode.end2.start: "start" is required
 	roles.Jedi.darkSide: "darkSide" is not allowed
+	push.android.apiKey: "apiKey" is required
+`
+
+exports['schema validator service with invalid internal flex should fail 1'] = `
+
+	host: "host" is not allowed
 `
--- test/unit/lib/schema-validator.test.js
+++ test/unit/lib/schema-validator.test.js
@@ -20,6 +20,10 @@ const SchemaValidator = require('../../../lib/SchemaValidator');
 const validEnvAllOptions = require('../../fixtures/config-files/env-valid-all-options.json');
 const validEnvSomeOptions = require('../../fixtures/config-files/env-valid-some-options.json');
 const invalidEnv = require('../../fixtures/config-files/env-invalid.json');
+const validInternalFlexAllOptions = require('../../fixtures/config-files/flex-internal-valid-all-options.json');
+const validInternalFlexSomeOptions = require('../../fixtures/config-files/flex-internal-valid-some-options.json');
+const invalidInternalFlex = require('../../fixtures/config-files/flex-internal-invalid.json');
+const validExternalFlexAllOptions = require('../../fixtures/config-files/flex-external-valid-all-options.json');
 
 describe('schema validator', () => {
   describe('env', () => {
@@ -49,6 +53,45 @@ describe('schema validator', () => {
           return done(ex);
         }
 
+        done();
+      });
+    });
+  });
+
+  describe('service', () => {
+    it('with valid internal flex service with all options should succeed', (done) => {
+      SchemaValidator.validate(ConfigType.SERVICE, validInternalFlexAllOptions, null, (err) => {
+        expect(err).to.not.exist;
+        done();
+      });
+    });
+
+    it('with valid internal flex with some options should succeed', (done) => {
+      SchemaValidator.validate(ConfigType.SERVICE, validInternalFlexSomeOptions, null, (err) => {
+        expect(err).to.not.exist;
+        done();
+      });
+    });
+
+    it('with invalid internal flex should fail', (done) => {
+      SchemaValidator.validate(ConfigType.SERVICE, invalidInternalFlex, null, (err) => {
+        expect(err).to.exist;
+        expect(err.name).to.equal('ValidationError');
+
+        const actualMsg = err.message.replace(/\r\n/g, '\n');
+        try {
+          snapshot(actualMsg);
+        } catch (ex) {
+          return done(ex);
+        }
+
+        done();
+      });
+    });
+
+    it('with valid external flex service with all options should succeed', (done) => {
+      SchemaValidator.validate(ConfigType.SERVICE, validExternalFlexAllOptions, null, (err) => {
+        expect(err).to.not.exist;
         done();
       });
     });

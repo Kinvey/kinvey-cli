5b6e8d6929391b7615d3a3188e5a5afc270bbdca
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2019-09-18 17:13:22 +0300

    META-359: Validate files size when deploying a site

--- test/unit/lib/site-envs-service.test.js
+++ test/unit/lib/site-envs-service.test.js
@@ -24,7 +24,10 @@ const KinveyError = require('../../../lib/KinveyError');
 const { assertions } = require('../../TestsHelper');
 
 const sandbox = sinon.createSandbox({});
+const siteDirectoryRelativePath = './test/fixtures/sites';
 const indexPageDefault = 'index.html';
+const maxSizeAllFilesDefault = Infinity;
+const maxSizeSingleFileDefault = Infinity;
 
 describe('SiteEnvsService', () => {
   describe('buildFormData', () => {
@@ -39,7 +42,7 @@ describe('SiteEnvsService', () => {
     it('with relative path to existent file should build correct form data', (done) => {
       const targetFile = 'index.html';
       const pathToTarget = `./test/fixtures/sites/${targetFile}`;
-      SiteEnvsService.buildFormData(pathToTarget, targetFile, null, (err, actual) => {
+      SiteEnvsService.buildFormData(pathToTarget, targetFile, null, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err, actual) => {
         if (err) {
           return done(err);
         }
@@ -54,7 +57,7 @@ describe('SiteEnvsService', () => {
 
     it('with relative path to existent directory should build correct form data', (done) => {
       const pathToTarget = './test/fixtures/sites';
-      SiteEnvsService.buildFormData(pathToTarget, indexPageDefault, null, (err, actual) => {
+      SiteEnvsService.buildFormData(pathToTarget, indexPageDefault, null, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err, actual) => {
         if (err) {
           return done(err);
         }
@@ -71,7 +74,7 @@ describe('SiteEnvsService', () => {
 
     it('with relative path to non-existent directory should return error', (done) => {
       const pathToTarget = './test/fixtures/sites/no-such-dir';
-      SiteEnvsService.buildFormData(pathToTarget, indexPageDefault, null, (err) => {
+      SiteEnvsService.buildFormData(pathToTarget, indexPageDefault, null, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err) => {
         const expectedErr = new KinveyError('InvalidPath', `Path '${pathToTarget}' does not exist.`);
         assertions.assertError(err, expectedErr);
         done();
@@ -81,7 +84,7 @@ describe('SiteEnvsService', () => {
     it('with absolute path to existent file should build correct form data', (done) => {
       const targetFile = 'index.html';
       const pathToTarget = path.resolve(`./test/fixtures/sites/${targetFile}`);
-      SiteEnvsService.buildFormData(pathToTarget, targetFile, null, (err, actual) => {
+      SiteEnvsService.buildFormData(pathToTarget, targetFile, null, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err, actual) => {
         if (err) {
           return done(err);
         }
@@ -97,7 +100,7 @@ describe('SiteEnvsService', () => {
     it('with valid path when indexPage is missing should return error', (done) => {
       const pathToTarget = './test/fixtures/sites';
       const expectedIndexPage = 'main.html';
-      SiteEnvsService.buildFormData(pathToTarget, expectedIndexPage, null, (err) => {
+      SiteEnvsService.buildFormData(pathToTarget, expectedIndexPage, null, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err) => {
         expect(err).to.exist;
         expect(err.name).to.equal('FilesNotFound');
         expect(err.message).to.equal(`Required file(s) not found.${EOL}\tIndex page: '${expectedIndexPage}'.`);
@@ -109,7 +112,7 @@ describe('SiteEnvsService', () => {
     it('with valid path when errorPage is missing should return error', (done) => {
       const pathToTarget = './test/fixtures/sites';
       const expectedErrorPage = 'my-error.html';
-      SiteEnvsService.buildFormData(pathToTarget, indexPageDefault, expectedErrorPage, (err) => {
+      SiteEnvsService.buildFormData(pathToTarget, indexPageDefault, expectedErrorPage, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err) => {
         expect(err).to.exist;
         expect(err.name).to.equal('FilesNotFound');
         expect(err.message).to.equal(`Required file(s) not found.${EOL}\tError page: '${expectedErrorPage}'.`);
@@ -122,11 +125,45 @@ describe('SiteEnvsService', () => {
       const pathToTarget = './test/fixtures/sites';
       const expectedIndexPage = 'main.html';
       const expectedErrorPage = 'my-error.html';
-      SiteEnvsService.buildFormData(pathToTarget, expectedIndexPage, expectedErrorPage, (err) => {
+      SiteEnvsService.buildFormData(pathToTarget, expectedIndexPage, expectedErrorPage, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err) => {
         expect(err).to.exist;
         expect(err.name).to.equal('FilesNotFound');
         const expErrMsg = `Required file(s) not found.${EOL}\tIndex page: '${expectedIndexPage}'.${EOL}\tError page: '${expectedErrorPage}'.`;
         expect(err.message).to.equal(expErrMsg);
+
+        done();
+      });
+    });
+
+    it('with valid path and file limits not exceeded should build correct form data', (done) => {
+      const pathToTarget = path.resolve('./test/fixtures/sites');
+      SiteEnvsService.buildFormData(pathToTarget, null, null, maxSizeSingleFileDefault, maxSizeAllFilesDefault, (err, actual) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(actual).to.be.an('object');
+        expect(actual).to.have.keys(['index.html', 'resources/icon.png']);
+
+        done();
+      });
+    });
+
+    it('with valid path and limit for single file exceeded should return error', (done) => {
+      const maxSizeSingleFile = 3200;
+      SiteEnvsService.buildFormData(siteDirectoryRelativePath, null, null, maxSizeSingleFile, maxSizeAllFilesDefault, (err) => {
+        expect(err).to.exist;
+        expect(err.message).to.equal(`The maximum allowed file size is ${maxSizeSingleFile} bytes. 'resources/icon.png' is 3964 bytes.`);
+
+        done();
+      });
+    });
+
+    it('with valid path and limit for total files size exceeded should return error', (done) => {
+      const maxSizeAllFiles = 3970;
+      SiteEnvsService.buildFormData(siteDirectoryRelativePath, null, null, maxSizeSingleFileDefault, maxSizeAllFiles, (err) => {
+        expect(err).to.exist;
+        expect(err.message).to.equal(`The maximum allowed files size is ${maxSizeAllFiles} bytes.`);
 
         done();
       });

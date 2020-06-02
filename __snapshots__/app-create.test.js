ea0a6d96145a90cc70399d728c5c82ce14e657fa
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2020-06-02 18:32:20 +0300

    KDEV-680: Require org on app create

--- __snapshots__/app-create.test.js
+++ __snapshots__/app-create.test.js
@@ -1,4 +1,4 @@
-exports['app create with a name and existent org identifier (ID) should succeed 1'] = `
+exports['app create with a name and existent org identifier (ID) should succeed and output JSON 1'] = `
 [debug] Checking for package updates
 [debug] Using profile 'activeProfile'
 [debug] Using organization: f71b0d5e60684b48b8265e7fa50302b9
@@ -6,7 +6,11 @@ exports['app create with a name and existent org identifier (ID) should succeed 
 [debug] Response: GET http://localhost:3234/v3/organizations/f71b0d5e60684b48b8265e7fa50302b9 200
 [debug] Request:  POST http://localhost:3234/v3/apps
 [debug] Response: POST http://localhost:3234/v3/apps 201
-Created application: 885f5d307afd4168bebca1a64f815c1e
+{
+  "result": {
+    "id": "885f5d307afd4168bebca1a64f815c1e"
+  }
+}
 
 `
 
@@ -22,25 +26,10 @@ Created application: 885f5d307afd4168bebca1a64f815c1e
 
 `
 
-exports['app create with a name should succeed and output JSON 1'] = `
+exports['app create with a name without org should fail 1'] = `
 [debug] Checking for package updates
 [debug] Using profile 'activeProfile'
-[debug] Request:  POST http://localhost:3234/v3/apps
-[debug] Response: POST http://localhost:3234/v3/apps 201
-{
-  "result": {
-    "id": "885f5d307afd4168bebca1a64f815c1e"
-  }
-}
-
-`
-
-exports['app create with a name should succeed and output default format 1'] = `
-[debug] Checking for package updates
-[debug] Using profile 'activeProfile'
-[debug] Request:  POST http://localhost:3234/v3/apps
-[debug] Response: POST http://localhost:3234/v3/apps 201
-Created application: 885f5d307afd4168bebca1a64f815c1e
+[error] OrgRequired: Organization is required. Please set active org or use the --org option.
 
 `
 

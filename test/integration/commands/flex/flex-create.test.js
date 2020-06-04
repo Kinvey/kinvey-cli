420fb479a391099f56675c23c702e8fbbc8a1eb5
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2020-06-04 17:32:10 +0300

    KDEV-680: Adjust flex create command

--- test/integration/commands/flex/flex-create.test.js
+++ test/integration/commands/flex/flex-create.test.js
@@ -13,8 +13,6 @@
  * contents is a violation of applicable laws.
  */
 
-const async = require('async');
-
 const { APIRuntime, AppOptionsName, AuthOptionsNames, CommonOptionsNames, CLIRuntime, FlexOptionsNames, Namespace, OrgOptionsName } = require('./../../../../lib/Constants');
 const fixtureApp = require('./../../../fixtures/app.json');
 const fixtureOrg = require('./../../../fixtures/org.json');
@@ -38,7 +36,12 @@ describe(baseCmd, () => {
   const optionsForOrg = { [OrgOptionsName.ORG]: fixtureOrg.id };
   const optionsForSecret = { secret: 123 };
   const optionsForSecretAndApp = Object.assign({}, optionsForSecret, optionsForApp);
-  const jsonOptionsPlusSecretAndApp = Object.assign(jsonOptions, optionsForSecretAndApp);
+  const optionsForSecretAndOrg = Object.assign({}, optionsForSecret, optionsForOrg);
+  const jsonOptionsPlusSecretAndOrg = Object.assign({}, jsonOptions, optionsForSecretAndOrg);
+  const orgApiOptions = {
+    domainType: 'organizationId',
+    domainEntityId: fixtureOrg.id
+  };
 
   before((done) => {
     setup.clearGlobalSetup(null, done);
@@ -59,62 +62,55 @@ describe(baseCmd, () => {
       setup.clearGlobalSetup(null, done);
     });
 
-    it('with a name, secret, basic env vars and app should succeed and output default format', (done) => {
-      const options = Object.assign({ [FlexOptionsNames.ENV_VARS]: 'KEY_1=value1,KEY_2=value2' }, optionsForSecretAndApp);
-      testServiceCreate(options, defaultFlags, serviceName, { envVars: { KEY_1: 'value1', KEY_2: 'value2' } }, done);
+    it('with a name, secret, basic env vars and org should succeed and output default format', (done) => {
+      const options = Object.assign({ [FlexOptionsNames.ENV_VARS]: 'KEY_1=value1,KEY_2=value2' }, optionsForSecretAndOrg);
+      const apiOptions = Object.assign({}, { envVars: { KEY_1: 'value1', KEY_2: 'value2' } }, orgApiOptions);
+      testServiceCreate(options, defaultFlags, serviceName, apiOptions, done);
     });
 
-    it('with a name, secret, basic env vars, runtime and app should succeed and output default format', (done) => {
-      const options = Object.assign({ [FlexOptionsNames.ENV_VARS]: 'KEY_1=value1', [FlexOptionsNames.RUNTIME]: CLIRuntime.NODE8 }, optionsForSecretAndApp);
-      testServiceCreate(options, defaultFlags, serviceName, { envVars: { KEY_1: 'value1' }, runtime: APIRuntime.NODE8 }, done);
+    it('with a name, secret, basic env vars, runtime and org should succeed and output default format', (done) => {
+      const options = Object.assign({ [FlexOptionsNames.ENV_VARS]: 'KEY_1=value1', [FlexOptionsNames.RUNTIME]: CLIRuntime.NODE8 }, optionsForSecretAndOrg);
+      const apiOptions = Object.assign({}, { envVars: { KEY_1: 'value1' }, runtime: APIRuntime.NODE8 }, orgApiOptions);
+      testServiceCreate(options, defaultFlags, serviceName, apiOptions, done);
     });
 
-    it('with a name, app and invalid env vars should fail', (done) => {
-      const options = Object.assign({ [FlexOptionsNames.ENV_VARS]: 'KEY_1=value1,KEY_2=[3,5]' }, optionsForApp);
-      testServiceCreate(options, defaultFlags, serviceName, null, done);
+    it('with a name, org and invalid env vars should fail', (done) => {
+      const options = Object.assign({ [FlexOptionsNames.ENV_VARS]: 'KEY_1=value1,KEY_2=[3,5]' }, optionsForOrg);
+      testServiceCreate(options, defaultFlags, serviceName, orgApiOptions, done);
     });
 
-    it('with a name, app and invalid runtime should fail', (done) => {
-      const options = Object.assign({ [FlexOptionsNames.RUNTIME]: 'node8.11.1' }, optionsForApp);
-      testServiceCreate(options, defaultFlags, serviceName, null, done);
+    it('with a name, org and invalid runtime should fail', (done) => {
+      const options = Object.assign({ [FlexOptionsNames.RUNTIME]: 'node8.11.1' }, optionsForOrg);
+      testServiceCreate(options, defaultFlags, serviceName, orgApiOptions, done);
     });
 
-    it('with a name, secret, complex env vars and app should succeed and output JSON', (done) => {
+    it('with a name, secret, complex env vars and org should succeed and output JSON', (done) => {
       const envVarsValue = '"{\\"KEY_1\\": [1, 2, \\"three\\"], \\"KEY_2\\": \\"some value\\"}"';
-      const options = Object.assign({ [FlexOptionsNames.ENV_VARS_SET]: envVarsValue }, jsonOptionsPlusSecretAndApp);
-      testServiceCreate(options, null, serviceName, { envVars: { KEY_1: JSON.stringify([1, 2, 'three']), KEY_2: 'some value' } }, done);
+      const options = Object.assign({ [FlexOptionsNames.ENV_VARS_SET]: envVarsValue }, jsonOptionsPlusSecretAndOrg);
+      const apiOptions = Object.assign({}, { envVars: { KEY_1: JSON.stringify([1, 2, 'three']), KEY_2: 'some value' } }, orgApiOptions);
+      testServiceCreate(options, null, serviceName, apiOptions, done);
     });
 
     it('without a name should fail', (done) => {
-      testServiceCreate(optionsForSecretAndApp, defaultFlags, null, null, done);
+      testServiceCreate(optionsForSecretAndOrg, defaultFlags, null, orgApiOptions, done);
     });
 
     it('without a secret should succeed', (done) => {
-      const cmd = buildCmd(baseCmd, [serviceName], optionsForApp, CommonOptionsNames.VERBOSE);
+      const cmd = buildCmd(baseCmd, [serviceName], optionsForOrg, CommonOptionsNames.VERBOSE);
       const replacementObj = {
         oldValue: /Secret: \S{4,}/,
         newValue: 'Secret: auto-generated'
       };
-      execCmdWithAssertion(cmd, null, null, true, true, false, replacementObj, done);
+      execCmdWithAssertion(cmd, null, orgApiOptions, true, true, false, replacementObj, done);
     });
 
-    it('without an app and org should fail', (done) => {
+    it('without an org should fail', (done) => {
       testServiceCreate(optionsForSecret, null, serviceName, null, done);
-    });
-
-    it('with both app and org should fail', (done) => {
-      const optionsAppOrg = Object.assign({}, optionsForSecretAndApp, optionsForOrg);
-      testServiceCreate(optionsAppOrg, null, serviceName, null, done);
     });
   });
 
   describe('with one-time session', () => {
     it('with name, secret and org should succeed', (done) => {
-      const apiOptions = {
-        domainType: 'organizations',
-        domainEntityId: fixtureOrg.id
-      };
-
       const optionsOrgSecretAndCredentials = Object.assign(
         {},
         optionsForSecret,
@@ -125,7 +121,7 @@ describe(baseCmd, () => {
         }
       );
       const cmd = buildCmd(baseCmd, [serviceName], optionsOrgSecretAndCredentials, defaultFlags);
-      execCmdWithAssertion(cmd, null, apiOptions, true, true, false, null, done);
+      execCmdWithAssertion(cmd, null, orgApiOptions, true, true, false, null, done);
     });
   });
 });
